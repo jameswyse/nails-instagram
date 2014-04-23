36ffa0e022cde1b328a6e99943e3dc22bebde681
James Wyse <james@jameswyse.net>
2014-04-23 16:03:46 +1000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var ig = require('instagram-node').instagram();
+
+exports = module.exports = {};
+
+exports.name = 'instagram';
+exports.type = 'service';
+
+exports.register = function(app, options, next) {
+  ig.use(options);
+  app.service.register('instagram', ig);
+
+  next();
+};
